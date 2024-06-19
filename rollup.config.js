fe6f9e2c0114ec24355d778aa0e15dea485a263b
harries4206 <harries4206@gmail.com>
2024-06-19 17:08:16 +0800

    update rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,38 @@
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
-import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
-    input: "./src/index.js",
+    input: './src/index.js',
     output: [
       {
-        file: "dist/index.js",
-        format: "cjs",
+        file: 'dist/index.js',
+        format: 'cjs',
         sourcemap: true,
       },
       {
-        file: "dist/index.es.js",
-        format: "es",
-        exports: "named",
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
         sourcemap: true,
       },
     ],
     plugins: [
       PeerDepsExternalPlugin(),
-      resolve(),
+      resolve({
+        extensions: ['.js', '.jsx'],
+      }),
 
       babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/env", "@babel/preset-react"],
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react'],
       }),
       commonjs(),
       postcss(),
     ],
-    external: ["react", "react-dom"],
+    external: ['react', 'react-dom'],
   },
 ];
